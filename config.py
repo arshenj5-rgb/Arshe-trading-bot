383dd3f8ab2fcd43e6bca34f797406e9a11f8c91
arshenj5-rgb <arshenj5@gmail.com>
2026-08-16 17:16:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,37 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-@dataclass(frozen=True)
+
+@dataclass
 class Settings:
-    ws_url: str = os.getenv("DERIV_WS_URL", "wss://ws.derivws.com/websockets/v3")
-    app_id: str = os.getenv("DERIV_APP_ID", "")
-    symbol: str = os.getenv("SYMBOL", "1HZ100V")
-    timeframes: tuple[int, ...] = tuple(
-        int(x.strip()) for x in os.getenv("TIMEFRAMES", "60,300,900,3600").split(",") if x.strip()
+    ws_url: str = os.getenv(
+        "DERIV_WS_URL",
+        "wss://ws.derivws.com/websockets/v3"
     )
-    history_count: int = int(os.getenv("HISTORY_COUNT", "300"))
-    min_confidence: int = int(os.getenv("MIN_CONFIDENCE", "70"))
+
+    app_id: str = os.getenv(
+        "DERIV_APP_ID",
+        "1089"
+    )
+
+    symbol: str = os.getenv(
+        "SYMBOL",
+        "1HZ100V"
+    )
+
+    timeframes: tuple[int, ...] = tuple(
+        int(x.strip())
+        for x in os.getenv(
+            "TIMEFRAMES",
+            "60,300,900,3600"
+        ).split(",")
+        if x.strip()
+    )
+
+    history_count: int = int(
+        os.getenv("HISTORY_COUNT", "300")
+    )
+
+    min_confidence: int = int(
+        os.getenv("MIN_CONFIDENCE", "70")
+    )
